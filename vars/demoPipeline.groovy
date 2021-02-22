2b58f989c5b0ff4e4f33037c31181df1ece21132
sonal <sinha.sonal.kumar@gmail.com>
2021-02-21 17:49:38 -0700

    for sample generic pipeline

---
+++ vars/demoPipeline.groovy
@@ -0,0 +1,22 @@
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+   pipeline{
+    agent any
+    
+    stages{
+        
+        stage("checkout"){
+            
+            steps{
+                //gitUtils('https://github.com/sinhasonalkumar/microservices.git')
+                gitUtils(pipelineParams.sourceRepoURL)
+            }
+        }
+    }
+}
+}
